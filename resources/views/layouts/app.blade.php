f9da14590c80a9420daaf2e198c9f5ca2a6665d7
oka314 <61148080+oka314@users.noreply.github.com>
2020-05-07 19:46:01 +0900

    ログイン後

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link href="{{ asset('css/user.css') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
@@ -58,7 +59,7 @@
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
-                                        {{ __('Logout') }}
+                                        {{ __('ログアウト') }}
                                     </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', 'TopsController@top');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/post', 'PostsController@index');
