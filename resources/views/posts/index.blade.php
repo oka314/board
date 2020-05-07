e3ab17fc409f086de7014777587c95dd5c5061a1
oka314 <61148080+oka314@users.noreply.github.com>
2020-05-07 22:29:21 +0900

    ビュー作成

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/css/posts/index.css">
+  <title>Boad一覧</title>
+</head>
+<body>
+  <head>
+    <a href="/" class="title">Boad</a>
+    <div class="header-menu">
+      <a href="#" class="user menu-btn">おか</a>
+      <a href="#" class="post-new menu-btn">新規投稿</a>
+      <a href="#" class="end menu-btn">ログアウト</a>
+    </div>
+  </head>
+</body>
+</html>
