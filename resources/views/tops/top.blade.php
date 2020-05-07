833d4247524f71011e894437646914f0d7ed68cf
oka314 <61148080+oka314@users.noreply.github.com>
2020-05-07 18:34:58 +0900

    会員登録

---
+++ resources/views/tops/top.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="/css/top.css">
+  <link rel="stylesheet" href="/media/media.css">
+  <title>Board</title>
+</head>
+<body>
+  <h1>掲示板へようこそ</h1>
+  <ul>
+    <li><a href="/home">ログイン</a></li>
+    <li><a href="/register">会員登録</a></li>
+  </ul>
+</body>
+</html>
