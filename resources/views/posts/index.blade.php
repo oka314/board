224c6cba0f5e5f9ffcba99581f3e858db6d07d42
oka314 <61148080+oka314@users.noreply.github.com>
2020-05-08 16:14:40 +0900

    メディアクエリ

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -12,10 +12,10 @@
     <a href="/" class="title">Boad</a>
     <input class="search">
     <button class="search-btn">検索</button>
-    <div class="header-menu">
-      <label for="menu_bar01">メニュー</label>
-      <input type="checkbox" id="menu-bar01" class="accordion"/>
-        <ul id="links01">
+    <div class="header-menu accbox">
+      <label for="label1">メニュー</label>
+      <input type="checkbox" id="label1" class="accordion cssacc"/>
+        <ul id="accshow">
           <li><a href="#" class="user menu-btn">自分の投稿一覧</a></li>
           <li> <a href="#" class="post-new menu-btn">新規投稿</a></li>
           <li><a href="#" class="end menu-btn">ログアウト</a></li>
@@ -24,3 +24,47 @@
   </header>
 </body>
 </html>
+
+  <!--ラベル1-->
+    <label for="label1">クリックして表示1</label>
+    <input type="checkbox" id="label1" class="cssacc" />
+    <div class="accshow">
+      <!--ここに隠す中身-->
+      <p>
+        こんにちは1
+      </p>
+    </div> 
+
+    .accbox {
+      margin: 2em 0;
+      padding: 0;
+      max-width: 400px;
+  }
+  
+  
+  .accbox label {
+      display: block;
+      margin: 1.5px 0;
+      padding : 11px 12px;
+      color :#2f8fcf;
+      font-weight: bold;
+      background :#a4cbf3;
+      cursor :pointer;
+      transition: all 0.5s;
+  }
+  
+  .accbox label:hover {
+     
+  }
+  
+  .accbox input {
+      
+  }
+  
+  .accbox .accshow {
+      
+  }
+  
+  .cssacc:checked + .accshow {
+      
+  }
